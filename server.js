5d181f21bb4570fac7dd38e32ae31065baed3f0e
Scott Young <youngscottj@gmail.com>
2020-01-06 00:58:31 -0700

    add graphql config

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const expressGraphQL = require('express-graphql')
 
 const app = express();
+
+// routes graphql requests to be usable in graphiql interface
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}))
 
 app.listen(4000, () => {
   console.log('App launched on port 4000')
