e9f5f85c3f4d7798107229960e304272bd550a6b
Scott Young <youngscottj@gmail.com>
2020-01-06 03:22:21 -0700

    add to schema

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express();
 
 // routes graphql requests to be usable in graphiql interface
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true
-}))
+}));
 
 app.listen(4000, () => {
   console.log('App launched on port 4000')
-})
+});
