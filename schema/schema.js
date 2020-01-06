df7a64a8c2183b3a17bee6dd31e105868300d6e8
Scott Young <youngscottj@gmail.com>
2020-01-06 04:09:03 -0700

    add json server for api tests

--- schema/schema.js
+++ schema/schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const graphql = require('graphql')
 const {
   GraphQLObjectType,
@@ -5,12 +6,7 @@ const {
   GraphQLInt,
   GraphQLSchema
 } = graphql
-const _ = require('lodash')
 
-const users = [
-  { id: '23', firstName: 'Bill', age: 20 },
-  { id: '21', firstName: 'Samantha', age: 21 }
-]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -28,7 +24,8 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id })
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data)
       }
     }
   }
